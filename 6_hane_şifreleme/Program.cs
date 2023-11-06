af311bd5f77b3ebd8e4d7bac523bc3dc62c84516
yasintuncc <95648660+yasintuncc@users.noreply.github.com>
2023-11-06 18:50:00 +0300

    Add project files.

---
+++ 6_hane_şifreleme/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _6_hane_şifreleme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Random random = new Random();
+            int password = random.Next(100000, 1000000);
+
+            Console.WriteLine("Oluşturulan Şifre: " + password);
+
+            
+            for (int i = 10; i >= 0; i--)
+            {
+                
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine(" yeni şifreyi "  );
+            Console.WriteLine("Şifre süresi doldu. Yeni bir şifre oluşturmanız gerekir ");
+
+            Random random2 = new Random();
+            int password2 = random.Next(100000, 1000000);
+
+            Console.WriteLine("Oluşturulan Şifre: " + password2);
+
+            Console.ReadLine();
+        }
+    }
+ }
+
